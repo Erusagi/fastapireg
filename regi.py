71096bd7f224a49309490acfd057257473cb5e59
Erusagi <141950504+Erusagi@users.noreply.github.com>
2024-01-21 20:59:41 +0200

    Add files via upload

---
+++ regi.py
@@ -0,0 +1,13 @@
+from fastapi import HTTPException, status
+
+@app.post("/users/", status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=409, detail="Email already registered")
+    new_user = User(email=user.email)
+    new_user.set_password(user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
